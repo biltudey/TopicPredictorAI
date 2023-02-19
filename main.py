85f48c39d192ea4876cc7ed891dd30cb0d732085
Biltu Dey <94779899+biltudey@users.noreply.github.com>
2023-02-19 19:06:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import OneHotEncoder
 import random
-import warnings
-warnings.filterwarnings('ignore')
+tf.keras.utils.disable_interactive_logging()
+st.set_page_config(layout="wide")
 
 
 target = ['BACKGROUND','CONCLUSIONS', 'METHODS', 'OBJECTIVE','RESULTS']
@@ -82,27 +82,111 @@ def printText(pred_label,test_sentence):
     if CONCLUSIONS != " ":
         st.write(f"**CONCLUSIONS** :\n {CONCLUSIONS}\n")
 
+def model(raw_text):
+    
+    text = raw_text.split(". ")
+    data = preprocess(text)
+    st.success('Text is preprocesed.', icon="✅")
+    test_dataset = convartTotensor(data)
+    st.success('Dataset is created.', icon="✅")
+    # st.write(test_dataset)
+    st.success('Model is working the data', icon="⏳")
+    predict = prediction(test_dataset)
+            
+
+    printText(predict,text)
+
+def aboutMe():
+    st.subheader("About me")
+
+    me = "### <b>Hi I am Biltu Dey. Currently I am a student. I am doing this project for educational Purpose.</b>"
+
+    st.markdown(me,unsafe_allow_html=True)
+
+    follow = """
+    If you liked the project you can follow me on social media.
+
+    [![linkedin](https://img.shields.io/badge/linkedin-0A66C2?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/BiltuDey/)
+    [![twitter](https://img.shields.io/badge/twitter-1DA1F2?style=for-the-badge&logo=twitter&logoColor=white)](https://twitter.com/mr_biltu)
+    
+
+    ### check [![github](https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white)](https://twitter.com/mr_biltu) account for outher project.
+
+    ## Support
+    Any kind of help or facing any problem feel free to contact me.
+
+    For support, email biltudey222@gmail.com.
+    """
+    st.markdown(follow,unsafe_allow_html=True)
 
 
 
 def main():
+    """
+        This is main Function
+    
+    """
     st.title("TopicPredictorAI")
-    st.subheader("Enter The Text")
-    raw_text = st.text_area(" ",)
-        
-    if st.button("Skim"):
-        text = raw_text.split(". ")
+    try:
+        font_css = """
+        <style>
+        button[data-baseweb="tab"] > div[data-testid="stMarkdownContainer"] > p {
+        font-size: 24px;
+        }
+        </style>
+        """
 
-        data = preprocess(text)
-        st.success('Text is preprocesed.', icon="✅")
-        test_dataset = convartTotensor(data)
-        st.success('Dataset is created.', icon="✅")
-        # st.write(test_dataset)
-        st.success('Model is working the data', icon="⏳")
-        predict = prediction(test_dataset)
+        st.write(font_css, unsafe_allow_html=True)
+        button_style = """
+            <style>
+            .stButton > button {
+                color: white;
+                background: black;
+                width: 100px;
+                height: 50px;
+                font: 50px;
+            }
+            </style>
+
+
+            """
+        st.markdown(button_style, unsafe_allow_html=True)
+
+        tab1, tab2,  tab3 = st.tabs(["AI","About Me" ,"How to use"])
+
+        with tab1:
+            st.subheader("Enter The Text")
+            raw_text = st.text_area(" ",value='', height=None, max_chars=None, key=None)
+            
+            if st.button("Skim"):
+                if raw_text.strip() == "":
+                    st.error("Please enter something")
+                else:
+                    model(raw_text)
+        with tab2:
+            aboutMe()
+        with tab3:
+            text = """
+                        ### **Enter your abstract text data.**
+                        #### Something like this.
+                """
+            st.markdown(text,unsafe_allow_html=True)
+            st.image('screenshot\Screenshot.png')
+            text = """
+                        ### **Then click the skim button.**
+                        #### Wait while text is processed by the model.
+                        #### After few second your data will look like this.
+                """
+            st.markdown(text,unsafe_allow_html=True)
+
+            st.image('screenshot\Screenshot1.png')
+
+
         
 
-        printText(predict,text)
+        
+    except:
+        st.error("There is an error. Please check your text data.")
 
         # st.write(predict)
 
